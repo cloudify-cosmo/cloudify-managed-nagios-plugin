48a215dc281930be0d25872b424c5f0249e03b48
kfrukacz-gs <krzysztof.frukacz@gigaspaces.com>
2021-03-01 10:05:25 +0100

    Increase delay (#11)
    * Add oprion to ignore failed checks for CheckGroup
    
    On failed retrieval the value of aggregate is 0
    
    * Changelog typo fix, update spec version
    
    * Workaround for nagios not reloading config
    
    CYBL-1105 workaround
    
    Co-authored-by: Krzysztof Frukacz <krzysztof.frukacz@cloudify.co>
    Co-authored-by: Ahmad Musa <53178237+ahmadiesa-abu@users.noreply.github.com>

--- integration_tests/test_groups.py
+++ integration_tests/test_groups.py
@@ -66,6 +66,49 @@ def test_scale_down_on_group_threshold_breach():
 
     main_client.tenants.delete(tenant)
 
+def test_scale_up_on_group_threshold_breach():
+    tenant = 'test_group_breach_scale_up'
+    config = utils.load_config()
+    main_client = utils.get_rest_client_using_config(
+        config,
+        tenant='default_tenant',
+    )
+
+    main_client.tenants.create(tenant)
+    client = utils.get_rest_client_using_config(
+        config,
+        tenant=tenant,
+    )
+
+    utils.upload_config_secrets(config, client)
+    installed_plugins = utils.upload_required_plugins(client)
+
+    utils.deploy_nagios(
+        utils.get_examples_blueprint_path('nagios-groups.yaml'),
+        utils.get_nagios_inputs(config),
+        client,
+    )
+    utils.install_blueprint(
+        utils.get_examples_blueprint_path('dummygroup.yaml'),
+        utils.get_monitored_vms_inputs(config),
+        'dummygroup',
+        client,
+    )
+
+    # Confirm the expected workflow runs
+    utils.wait_for_execution_on_deployment('scale',
+                                           'dummygroup',
+                                           client)
+
+    utils.remove_deployment('dummygroup', client)
+    utils.delete_blueprint('dummygroup', client)
+
+    utils.remove_nagios(client)
+    utils.delete_plugins(installed_plugins, client)
+    utils.remove_config_secrets(client)
+
+    main_client.tenants.delete(tenant)
+
 
 def test_scale_down_on_metagroup_threshold_breach():
     tenant = 'test_metagroup_breach_scale_down'
