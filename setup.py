fcbe5d2382ccf3688c97c08df9a68486f5df6518
kaplanyaniv <yaniv.224@gmail.com>
2022-08-11 10:35:26 +0300

    update to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import os
 from setuptools import setup
+from setuptools import find_packages
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
 
 
 def get_resources():
@@ -12,9 +19,20 @@ def get_resources():
     return paths
 
 
+def get_version(rel_file='plugin.yaml'):
+    lines = read(rel_file)
+    for line in lines.splitlines():
+        if 'package_version' in line:
+            split_line = line.split(':')
+            line_no_space = split_line[-1].replace(' ', '')
+            line_no_quotes = line_no_space.replace('\'', '')
+            return line_no_quotes.strip('\n')
+    raise RuntimeError('Unable to find version string.')
+
+
 setup(
     name='cloudify-managed-nagios-plugin',
-    version='1.0.13',
+    version=get_version(),
     packages=[
         'managed_nagios_plugin',
         'managed_nagios_plugin.check',
@@ -23,7 +41,7 @@ setup(
         'managed_nagios_plugin.snmp_trap',
         'managed_nagios_plugin.target_type',
     ],
-    install_requires=['cloudify-common>=5.2',
+    install_requires=['cloudify-common>=5.0.5',
                       'Jinja2>=2.7.2',
                       'future'],
     package_data={'managed_nagios_plugin': get_resources()},
